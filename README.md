1bb919f6650a5d853147a354502cbda0a11cc8c8
oskytar <oscar.miras@tutor.bit.es>
2020-12-14 19:41:14 +0100

    Enunciado modificado

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
+# El botón cambia-estilos
+
+Queremos crear script que, al hacer clic en el botón **Click Me!** cambie el el color de fondo de todo el elemento __body__.
+
+Se recomienda impelementar dos versiones:
+
+1. Al hacer clic en el botón "Click Me!" el color del fondo de __body__ siempre cambia al mismo color, por ejemplo, amarillo
+
+2. Implementar el mismo comportamiento del punto uno, pero ahora, el color debe ser tomado de forma aleatoria del array __colors__; presente en el fichero script.js.
+
+Implementa tu solución en el fichero **script.js**
+
 ## Demo del resultado
 [Demo](https://js-beginners.github.io/project_change_color_background/)
--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
 //Choose a random color
-const button = document.querySelector('button')
-const body = document.querySelector('body')
 const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
 
-body.style.backgroundColor = 'violet'
-button.addEventListener('click', changeBackground)
 
-function changeBackground(){
-const colorIndex= parseInt(Math.random()*colors.length)
-body.style.backgroundColor = colors[colorIndex]
+/**
+ * 1. Necesitamos crear una función que devuelva un color aleatorio entre todos los colores del array 'colors'. Crea esta función o mira por Internet como hacer para escoger una posición aleatorio de un array
+ */
+
+function getRandomColor() {
+    // Si bien normalmente las funciones no conocen nada del exterior; estos casos son una buena excepción. La lista de colores se puede considerar una cofiguración "global" a la cual cualqueir función debe poder acceder.
+
+    console.log(colors)
 }
+
+/**
+ * 2. Debemos seleccionar el botón a clicar, y todo el elemento 'body'. Guarda cada uno de los elementos en una variable. Deberían ser tan solo dos líneas de código. Muestra por consola el valor de las variables para comprobar que has seleccionado los elementos adecuados
+ */
+
+// let body ....
+// let button ...
+
+ /**
+  * 3. Asocia al botón 'button' un evento onclick. Dicho evento debe llamar a una función; que se va a encargar de:
+  * 
+  *   A) Llamar a la otra función 'getRandomColor' y obtener un color aleatorio
+  *    B) Modificar el 'backgroundColor' del body con el valor que hemos conseguido. Deves investigar como cambiar el color del fondo de un elemento HTML a través de JavaScript
+  */
